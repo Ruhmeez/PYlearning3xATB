9e89388554ded4a723e830e29fd2ed571c00825a
testing <ruheenfirdoseee@gmail.com>
2024-06-11 23:08:29 +0530

    assignment tasks

---
+++ Ex_10062024/Task1.py
@@ -0,0 +1,26 @@
+#Task #1
+# a. Explain the difference between the = operator and the == operator in Python.
+# Ans: "=" is an Assignment operator used to assign value to the variable.
+#     "==" is a relational operator used to check the equality of two values.
+
+# b. What does the ** operator do in Python, and how is it used?
+# Ans: "**" is called Exponentiation operator, used to get the power of the given number.
+# Eg: 4**2 -> 4 is base and 2 is power, the result is 16
+
+
+# c. What does the ^ operator do in Python, and in what context is it commonly used?
+# The bitwise XOR (exclusive OR) operator compares each bit of two integers and returns a new integer with bits set to 1 if the corresponding bits in the operands are different. It performs a bitwise "exclusive or" operation.
+# Ex:
+# a = 5   # binary: 0101
+# b = 3   # binary: 0011
+# result = a ^ b  # binary result: 0110 (decimal 6)
+
+
+
+
+
+
+
+
+
+
---
+++ Ex_10062024/Task2.py
@@ -0,0 +1,22 @@
+# Task #2
+
+#Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+
+#Program:
+num = 2
+square_num = print(num**2)
+cube_num = print(num**3)
+
+
+#Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+
+#Program:
+
+x = input("Enter first number:\n")
+y = input("Enter second number:\n")
+
+print("x is greater than y" if x>y else "y is greater than x")
+print("x is equal to y" if x==y else "x is not equal to y")
+
+
+
